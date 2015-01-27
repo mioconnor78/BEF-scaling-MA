bc52522e8b525882e042577be46db60f9fefa91e
Mary <oconnor@zoology.ubc.ca>
2015-01-27 07:00:59 -0800

    made a 2-panel figure 2 that includes basic model coefs

--- figure 2 codeA.R
+++ figure 2 codeA.R
@@ -12,35 +12,135 @@
 ## MO: I know how painful and inefficient this code is! I would love it (and learn from it) if someone felt like presenting an alternative approach to generating this figure. I know there are better ways.
 
 
-par(
-  family = "serif",  
-  oma = c(0,0,0,0),  # Since it is a single plot, I set the outer margins to zero.
-  #mar = c(5,8,4,2),  # Inner margins are set through a little trial and error.
-  mfcol = c(2,2)
-)
-
 #layout(matrix(c(1,2,3,4)), 1, 1, byrow = FALSE)
 
 # Figure 2A: SST4 slopes
+# create file for model averaged estimates
 estimates <- as.data.frame(m.avg[3])
 estimates$slint <- c('I', 'S', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'I', 'I','S', 'S')
 rownames(estimates) <- c('Intercept', 'ln(S)', 'ln(Duration)', 'Ecosystem', 'Herbivore', 'Detritivore', '+Consumer', 'ln(S)*ln(Duration)', 'ln(S)*Ecosystem', 'ln(S)*Herbivore', 'ln(S)*Detritivore', 'ln(S)* +Consumer', '+Resource', '-Resource', 'ln(S) * +Resource', 'ln(S)* -Resource')
 est.sl <- estimates[estimates$slint == 'S',]
 est.int <- estimates[estimates$slint == 'I',]
 
-est.B <- as.data.frame(as.numeric(round(fixef(modBasic),3)))
-est.B$se <- as.numeric(round(sqrt(diag(vcov(modBasic))),3))
+# create data for best model estimates
+est.B <- as.data.frame(as.numeric(round(fixef(modBtrophic),3)))
+est.B$se <- as.numeric(round(sqrt(diag(vcov(modBtrophic))),3))
 names(est.B) <- c('est', 'se')
 est.B <- rbind(est.B, c('',''))
 est.B <- rbind(est.B, c('',''))
 est.B <- rbind(est.B, c('',''))
 est.B <- rbind(est.B, c('',''))
-est.B <- rbind(est.B, c('',''))
-row.names(est.B) <- c('Intercept', 'ln(S)', 'ln(Duration)', 'Ecosystem', 'Herbivore', 'Predator', 'Detritivore', '+Consumer', 'ln(S)*ln(Duration)', 'ln(S)*Ecosystem', 'ln(S)*Herbivore', 'ln(S)*Predator','ln(S)*Detritivore', 'ln(S)* +Consumer', '+Resource', '-Resource', 'ln(S) * +Resource', 'ln(S)* -Resource', 'ln(MaxDur)', 'ln(S)*ln(MaxDur)')
-est.B$slint <- c('I', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'I', 'I','S', 'S', 'I', 'S')
-
+#est.B <- rbind(est.B, c('',''))
+rownames(est.B) <- c('Intercept', 'ln(S)', 'ln(Duration)', 'Ecosystem', 'Herbivore', 'Detritivore', '+Consumer', 'ln(S)*ln(Duration)', 'ln(S)*Ecosystem', 'ln(S)*Herbivore', 'ln(S)*Detritivore', 'ln(S)* +Consumer', '+Resource', '-Resource', 'ln(S) * +Resource', 'ln(S)* -Resource')
+est.B$slint <- c('I', 'S', 'I', 'I', 'I', 'I', 'I',  'S', 'S', 'S', 'S', 'S',  'I', 'I','S', 'S')
 est.B.sl <- est.B[est.B$slint == 'S',]
 est.B.int <- est.B[est.B$slint == 'I',]
+
+
+# create data for basic model estimates
+est.Ba <- as.data.frame(as.numeric(round(fixef(modBasic),3)))
+est.Ba$se <- as.numeric(round(sqrt(diag(vcov(modBasic))),3))
+names(est.Ba) <- c('est', 'se')
+est.Ba <- rbind(est.Ba[1:3,], c('',''), c('',''), c('',''), c('',''), est.Ba[4:nrow(est.Ba),], c('',''), c('',''), c('',''), c('',''), c('',''), c('',''), c('',''), c('','')) 
+rownames(est.Ba) <- c('Intercept', 'ln(S)', 'ln(Duration)', 'Ecosystem', 'Herbivore', 'Detritivore', '+Consumer', 'ln(S)*ln(Duration)', 'ln(S)*Ecosystem', 'ln(S)*Herbivore', 'ln(S)*Detritivore', 'ln(S)* +Consumer', '+Resource', '-Resource', 'ln(S) * +Resource', 'ln(S)* -Resource')
+est.Ba$slint <- c('I', 'S', 'I', 'I', 'I', 'I','I', 'S', 'S', 'S', 'S', 'S',  'I', 'I','S', 'S')
+est.Ba.sl <- est.Ba[est.Ba$slint == 'S',]
+est.Ba.int <- est.Ba[est.Ba$slint == 'I',]
+
+### two-paneled figure
+
+par(
+  family = "serif",  
+  oma = c(0,0,0,0),  # Since it is a single plot, I set the outer margins to zero.
+  #mar = c(5,8,4,2),  # Inner margins are set through a little trial and error.
+  mfcol = c(1,2)
+)
+
+#TOP PANEL: SLOPES
+
+par(mar=(c(5,9.5,4,0)))
+plot(NULL,                                
+     xlim = c(-0.3, 0.4),                        	
+     ylim = c(.7, length(est.sl[,1]) + .3), 	
+     axes = F, xlab = NA, ylab = NA)
+
+# add the data
+est <- as.numeric(est.sl[,1]) 
+se <- as.numeric(est.sl[,2] )                                         
+ests.B <- as.numeric(est.B.sl[,1])
+ses.B <- as.numeric(est.B.sl[,2])
+ests.Ba <- as.numeric(est.Ba.sl[,1])
+ses.Ba <- as.numeric(est.Ba.sl[,2])
+var.names<-rownames(est.B.sl)
+
+b <- 0.2
+for (i in 1:length(est)) {                                            
+  points(est[i], i, pch = 19, cex = 1.2)                              
+  points(ests.B[i], i+b, pch = 19, cex = 1.2, col = 'gray50') 
+  points(ests.Ba[i], i+2*b, pch = 19, cex = 1.2, col = 'gray75') 
+  lines(c(est[i] + 1.94*se[i], est[i] - 1.94*se[i]), c(i, i), lwd = 2)         # add 90% CIs
+  lines(c(ests.B[i] + 1.94*ses.B[i], ests.B[i] - 1.94*ses.B[i]), c(i+b, i+b), col = 'gray50', lwd = 2)
+  lines(c(ests.Ba[i] + 1.94*ses.Ba[i], ests.Ba[i] - 1.94*ses.Ba[i]), c(i+2*b, i+2*b), col = 'gray75', lwd = 2)
+  text(-.5, i, adj = c(1,0), var.names[i], xpd = T, cex = 1)        # add the variable names
+  text(0.35, length(est.B.sl[,1]), 'A', cex = 1.5)
+}
+
+# add axes and labels
+axis(side = 1)                                                                                         
+abline(v = 0, lty = 3, col = "grey40")                                                                   
+mtext(side = 1, "Slope coefficients", line = 3)                                              
+mtext(side = 3, "ModBtrophic and Avgd model", line = 1)   # add title
+box()                                          
+
+
+par(mar=(c(5,5.5,4,4)))
+plot(NULL,                                
+     xlim = c(-3, 6),                          
+     ylim = c(.7, length(est.sl[,1]) + .3), 	
+     axes = F, xlab = NA, ylab = NA)
+
+# add the data
+est <- as.numeric(est.int[,1]) 
+se <- as.numeric(est.int[,2] )                                         
+ests.B <- as.numeric(est.B.int[,1])
+ses.B <- as.numeric(est.B.int[,2])
+ests.Ba <- as.numeric(est.Ba.int[,1])
+ses.Ba <- as.numeric(est.Ba.int[,2])
+var.names<-rownames(est.B.int)
+
+b <- 0.2
+for (i in 1:length(est)) {                                            
+  points(est[i], i, pch = 19, cex = 1.2)                              
+  points(ests.B[i], i+b, pch = 19, cex = 1.2, col = 'gray50') 
+  points(ests.Ba[i], i+2*b, pch = 19, cex = 1.2, col = 'gray75') 
+  lines(c(est[i] + 1.94*se[i], est[i] - 1.94*se[i]), c(i, i), lwd = 2)         # add 90% CIs
+  lines(c(ests.B[i] + 1.94*ses.B[i], ests.B[i] - 1.94*ses.B[i]), c(i+b, i+b), col = 'gray50', lwd = 2)
+  lines(c(ests.Ba[i] + 1.94*ses.Ba[i], ests.Ba[i] - 1.94*ses.Ba[i]), c(i+2*b, i+2*b), col = 'gray75', lwd = 2)
+  #text(-.5, i, adj = c(1,0), var.names[i], xpd = T, cex = 1)        # add the variable names
+  text(6, length(est.B.int[,1]), 'B', cex = 1.5)
+}
+
+# add axes and labels
+axis(side = 1)                                                                                         
+abline(v = 0, lty = 3, col = "grey40")                                                                   
+mtext(side = 1, "Intercept coefficients", line = 3)                                              
+mtext(side = 3, "ModBtrophic and Avgd model", line = 1)   # add title
+box()                    
+
+
+
+
+
+
+
+#### four paneled figure
+
+par(
+  family = "serif",  
+  oma = c(0,0,0,0),  # Since it is a single plot, I set the outer margins to zero.
+  #mar = c(5,8,4,2),  # Inner margins are set through a little trial and error.
+  mfcol = c(2,2)
+)
 
 #par(mar=(c(5,7.5,4,0)))
 par(mar=(c(5,9.5,4,0)))
@@ -70,7 +170,7 @@ for (i in 1:length(est)) {
 axis(side = 1)                                                                                         
 abline(v = 0, lty = 3, col = "grey40")                                                                   
 mtext(side = 1, "Slope coefficients", line = 3)                                              
-mtext(side = 3, "Predator Studies Included", line = 1)   # add title
+mtext(side = 3, "ModBtrophic and Avgd model", line = 1)   # add title
 box()                                          
 
 ## Panel C: SST4 Ints ###
@@ -106,37 +206,8 @@ box()
 
 
 ## Panel B
-# SST5 slopes
-estimates <- as.data.frame(m.avg.5[3])
-estimates <- rbind(estimates[1:5,], c('','','','','',''), estimates[6:nrow(estimates),])  #these rows only needed for SST5 (no carnivores)
-estimates <- rbind(estimates[1:11,], c('','','','','',''), estimates[12:nrow(estimates),])
-estimates$slint <- c('I', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'I', 'I','S', 'S', 'I', 'S')
-rownames(estimates) <- c('Intercept', 'ln(S)', 'ln(Duration)', 'Ecosystem', 'Herbivore', 'Predator', 'Detritivore', '+Consumer', 'ln(S)*ln(Duration)', 'ln(S)*Ecosystem', 'ln(S)*Herbivore', 'ln(S)*Predator','ln(S)*Detritivore', 'ln(S)* +Consumer', '+Resource', '-Resource', 'ln(S) * +Resource', 'ln(S)* -Resource', 'ln(MaxDuration)', 'ln(S)*ln(MaxDuration)')
-est.sl <- estimates[estimates$slint == 'S',]
-est.int <- estimates[estimates$slint == 'I',]
 
-est.B <- as.data.frame(as.numeric(round(fixef(modBtrophic5),3)))
-est.B$se <- as.numeric(round(sqrt(diag(vcov(modBtrophic5))),3))
-names(est.B) <- c('est', 'se')
-
-#the parameters in estimates and est.B have to line up
-rownames(est.B)
-rownames(estimates)
-
-est.B <- rbind(est.B, c('',''))
-est.B <- rbind(est.B, c('',''))
-est.B <- rbind(est.B, c('',''))
-est.B <- rbind(est.B, c('',''))
-est.B <- rbind(est.B, c('',''))
-est.B <- rbind(est.B, c('',''))
-est.B <- rbind(est.B[1:5,], c('','','','','',''), est.B[6:nrow(est.B),]) #these rows only needed for SST5 (no carnivores)
-est.B <- rbind(est.B[1:11,], c('','','','','',''), est.B[12:nrow(est.B),])
-row.names(est.B) <- c('Intercept', 'ln(S)', 'ln(Duration)', 'Ecosystem', 'Herbivore', 'Predator', 'Detritivore', '+Consumer', 'ln(S)*ln(Duration)', 'ln(S)*Ecosystem', 'ln(S)*Herbivore', 'ln(S)*Predator','ln(S)*Detritivore', 'ln(S)* +Consumer', '+Resource', '-Resource', 'ln(S) * +Resource', 'ln(S)* -Resource''ln(MaxDur)', 'ln(S)*ln(MaxDur)')
-est.B$slint <- c('I', 'S', 'I', 'I', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'S', 'S', 'S', 'I', 'I','S', 'S', 'I','S')
-est.B.sl <- est.B[est.B$slint == 'S',]
-est.B.int <- est.B[est.B$slint == 'I',]
-
-
+# Basic model
 par(mar=(c(5,5.5,4,4)))
 plot(NULL,                                
      xlim = c(-0.4, 0.6),                        	
@@ -146,9 +217,9 @@ plot(NULL,
 # add the data
 est <- as.numeric(est.sl[,1]) 
 se <- as.numeric(est.sl[,2] )                                         
-ests.B <- as.numeric(est.B.sl[,1])
-ses.B <- as.numeric(est.B.sl[,2])
-var.names<-rownames(est.B.sl)
+ests.B <- as.numeric(est.Ba.sl[,1])
+ses.B <- as.numeric(est.Ba.sl[,2])
+var.names<-rownames(est.Ba.sl)
 
 b <- 0.2
 for (i in 1:length(est)) {                                            
@@ -164,7 +235,7 @@ for (i in 1:length(est)) {
 axis(side = 1)                                                                                      
 abline(v = 0, lty = 3, col = "grey40")                                                                   
 mtext(side = 1, "Slope coefficients", line = 3)                                             
-mtext(side = 3, "Predator Studies Excluded", line = 1)   # add title
+mtext(side = 3, "Basic model and Avgd model", line = 1)   # add title
 box()                            
 
 
@@ -178,9 +249,9 @@ plot(NULL,
 # add the data
 est <- as.numeric(est.int[,1]) 
 se <- as.numeric(est.int[,2] )                                         
-ests.B <- as.numeric(est.B.int[,1])
-ses.B <- as.numeric(est.B.int[,2])
-var.names<-rownames(est.B.int)
+ests.B <- as.numeric(est.Ba.int[,1])
+ses.B <- as.numeric(est.Ba.int[,2])
+var.names<-rownames(est.Ba.int)
 
 b <- 0.2
 for (i in 1:length(est)) {                                            
