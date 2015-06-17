a6f77836e259f7911785367590cdc5f9781a4668
Mary <oconnor@zoology.ubc.ca>
2015-06-17 08:35:54 -0700

    no major changes; this produces different plots on my laptop vs computer. maybe it's teh datafile?? not sure.

--- figure 2 caterpillar plots.R
+++ figure 2 caterpillar plots.R
@@ -6,21 +6,20 @@ library(arm)
 mod <- modBtrophic
 mod <- modBasic
 
-par(  oma = c(0,2,0,2),  # Since it is a single plot, I set the outer margins to zero.
+par(oma = c(0,2,0,2),  # Since it is a single plot, I set the outer margins to zero.
   #fin = c(7,5), pty = "m",
-  mar = c(5,10,4,0),  # Inner margins are set through a little trial and error.
-  mfcol = c(1,2)
+  #mar = c(5,10,4,0),  # Inner margins are set through a little trial and error.
+  mfcol = c(1,3)
 )
 
-
 # make the caterpillar plot for Entry level coefs
-cat.plot <- sjp.lmer(mod, type = 're', sort = "logSc", ri.nr = 1, fade.ns = TRUE, free.scale = TRUE, geom.colors = c(1, 1), showValueLabels = FALSE)
+cat.plot <- sjp.lmer(mod, sort = "logSc", ri.nr = 1, fade.ns = TRUE, free.scale = TRUE, geom.colors = c(1, 1), showValueLabels = FALSE)
 
 # make the caterpillar plot for ExptA level coefs
-cat.plot <- sjp.lmer(mod, sort = "logSc", ri.nr = 2, fade.ns = TRUE, free.scale = TRUE, geom.colors = c(1, 1), showValueLabels = FALSE)
+cat.plot2 <- sjp.lmer(mod, sort = "logSc", ri.nr = 2, fade.ns = TRUE, free.scale = TRUE, geom.colors = c(1, 1), showValueLabels = FALSE)
 
 # make the caterpillar plot for Study level coefs
-cat.plot <- sjp.lmer(mod, sort = "logSc", ri.nr = 3, fade.ns = TRUE, free.scale = TRUE, geom.colors = c(1, 1), showValueLabels = FALSE)
+cat.plot3 <- sjp.lmer(mod, sort = "logSc", ri.nr = 3, fade.ns = TRUE, free.scale = TRUE, geom.colors = c(1, 1), showValueLabels = FALSE)
 
 #this appears to maybe be just plotting ranefs based on expt-level random effects. 
 
