d071917e1f2515b7afbc8a241d554fdab71b481b
Mary <oconnor@zoology.ubc.ca>
2015-06-16 17:09:56 -0700

    caterpillar plots plot area

--- figure 2 caterpillar plots.R
+++ figure 2 caterpillar plots.R
@@ -6,6 +6,13 @@ library(arm)
 mod <- modBtrophic
 mod <- modBasic
 
+par(  oma = c(0,2,0,2),  # Since it is a single plot, I set the outer margins to zero.
+  #fin = c(7,5), pty = "m",
+  mar = c(5,10,4,0),  # Inner margins are set through a little trial and error.
+  mfcol = c(1,2)
+)
+
+
 # make the caterpillar plot for Entry level coefs
 cat.plot <- sjp.lmer(mod, type = 're', sort = "logSc", ri.nr = 1, fade.ns = TRUE, free.scale = TRUE, geom.colors = c(1, 1), showValueLabels = FALSE)
 
