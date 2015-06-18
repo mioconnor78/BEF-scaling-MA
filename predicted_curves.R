b381a7b536ed83979a44b1426ffc2865d51a75c8
Mary <oconnor@zoology.ubc.ca>
2015-06-17 17:48:02 -0700

    work with andrew

--- predicted_curves.R
+++ predicted_curves.R
@@ -7,9 +7,11 @@ data <- SST5 #<- read.csv("sst5_20150601_jeb.csv")
 #I want some new models!
 modBtrophic<-lmer(logY.rs ~ logSc*log(Tscale) + logSc*Sys1 + logSc*factor(TG1) + (1 + logSc|Entry) + (1 + logSc|ExptA) + (1 + logSc|Study), data=data, REML = FALSE, na.action=na.omit)
 modBtrophic2<-lmer(logY.rs ~ logSc+log(Tscale) + Sys1 + (1 + logSc|Entry) + (1 + logSc|ExptA) + (1 + logSc|Study), data=data, REML = FALSE, na.action=na.omit)
-modBtrophic3<-lmer(logY.rs ~ logSc*log(Tscale) + logSc*Sys1*factor(TG1) - logSc:Sys1T:factor(TG1)2 +
-                     (1 + logSc|Entry) + (1 + logSc|ExptA) + (1 + logSc|Study), data=data, REML = FALSE, na.action=na.omit)
 
+modBtrophic3 <- lmer(logY.rs ~ logSc*log(Tscale) + logSc*Sys1*factor(TG1) - logSc:Sys1T:factor(TG1)2 + (1 + logSc|Entry) + (1 + logSc|ExptA) + (1 + logSc|Study), data=data, REML = FALSE, na.action=na.omit)
+
+## MO use this model:
+modBtrophic3 <- lmer(logY.rs ~ logSc*log(Tscale) + logSc*Sys1*factor(TG1) - logSc:Sys1:factor(TG1) + (1 + logSc|Entry) + (1 + logSc|ExptA) + (1 + logSc|Study), data=data, REML = FALSE, na.action=na.omit)
 
 SST5$modBtrophic.fitted <- fitted(modBtrophic)
 SST5$modBtrophic3.fitted <- fitted(modBtrophic3)
@@ -21,7 +23,12 @@ SST5$modBtrophic3.fitted <- fitted(modBtrophic3)
 #####
 
 modBtrophicPred.varyNone <- data.frame(logSc=rep(log(seq(1,5,length.out=100))-log(8), 2), Sys1=factor(c(rep("A",100), rep("T",100))))
-modBtrophicPred.varyNone$logY.rs.predicted <-  fixef(modBtrophic)[1] + fixef(modBtrophic)[2]*modBtrophicPred.varyNone$logSc + mean(SST5$Tscale)*fixef(modBtrophic)[3]
+
+modBtrophicPred.varyNone <- data.frame(logSc=rep(log(seq(1,5,length.out=100))-log(8), 2), TG1=factor(c(rep("1",100), rep("2",100), rep("4",100))))
+
+
+modBtrophicPred.varyNone$logY.rs.predicted <-  fixef(modBtrophic3)[1] + fixef(modBtrophic3)[2]*modBtrophicPred.varyNone$logSc + mean(SST5$Tscale)*fixef(modBtrophic3)[3]
+
 modBtrophicPred.varyNone$logY.rs.predicted[101:200] <- modBtrophicPred.varyNone$logY.rs.predicted[101:200]+fixef(modBtrophic)[4]
 
 #FIG 1!!!!
@@ -34,7 +41,7 @@ fitted_plot + facet_grid(. ~ Sys1) +
 
 ###The master plot
 fitted_plot <- ggplot() +
-  geom_line(data=SST5, aes(x=exp(logSc), y=exp(modBtrophic.fitted), group=Entry), color="lightgrey") +
+  geom_line(data=SST5, aes(x=exp(logSc), y=exp(modBtrophic3.fitted), group=Entry), color="lightgrey") +
   theme_bw(base_size=17)
 fitted_plot
 
@@ -105,11 +112,26 @@ makeModB3Pred <- function(newdata, object=modBtrophic3){
 
 modBtrophic3Pred.varyall$logY.rs.predicted <- makeModB3Pred(modBtrophic3Pred.varyall)
 
-fitted_plot3 <- ggplot() +
-  geom_line(data=SST5, aes(x=exp(logSc), y=exp(modBtrophic3.fitted), group=Entry), color="lightgrey") +
+mean(rnorm(50))
+
+50 %>% 
+  rnorm %>% 
+  mean
+
+fitted_plot3 <- SST5 %>% 
+  mutate(Sys1 = as.character(Sys1),
+         Sys1 = ifelse(Sys1 == "T", "Terrestrial", "Aquatic")) %>%
+  ggplot(aes(x=exp(logSc),
+             y=exp(modBtrophic3.fitted),
+             group=Entry)) +
+  geom_line(color="black", alpha = "0.1", size = 1.5) +
   theme_bw(base_size=17) +
-  facet_grid(Sys1 ~ TG1)
+  facet_grid(Sys1 ~ TG1, scales = "free_y") +
+  ylab("trophic level") +
+  xlab("species richness")
 
 fitted_plot3+
-  geom_line(data=modBtrophic3Pred.varyall, mapping=aes(x=exp(logSc), y=exp(logY.rs.predicted)))
+  geom_line(data=modBtrophic3Pred.varyall, mapping=aes(x=exp(logSc), y=exp(logY.rs.predicted), group = NULL), colour = "red", size = 2)
 
+ggsave("bdef.png")
+getwd()
