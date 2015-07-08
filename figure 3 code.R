a424f87271a47a72cd8cc52663e768cb671ec75b
Mary <oconnor@zoology.ubc.ca>
2015-07-08 10:23:18 -0700

    saved open metamaster, i think the only change was sorting.
    wrestling with figure 3 code to make sure the coefs are right.

--- figure 3 code.R
+++ figure 3 code.R
@@ -9,47 +9,29 @@
 ##### Producing experiment-level slope estimates #########
 ##########################################################
 data <- SST5
-mod <- (mod4.1)
 
-#rand.cat <- data.frame(cbind(as.numeric(as.character(data$Entry)), as.numeric(as.character(data$ExptA)), as.numeric(as.character(data$Study))))
-#names(rand.cat) <- c('Entry', 'ExptA', 'Study')
-#rand.cat1 <- ddply(rand.cat, .(Entry, ExptA), summarize, mean(Study)) 
+mod4 <- lmer(logY.rs ~ logSc*Sys1*TG1 + log(Tscale) + (1 + logSc|Entry) + (1 + logSc|ExptA) + (1 + logSc|Study), data=data, REML = TRUE, na.action=na.omit)
 
-### for modBasic
-rand.cat <- ddply(data, .(Entry, ExptA, Study), summarize, mean(logY.rs))
-names(rand.cat) <- c('Entry','ExptA', 'Study', 'meanlogY')
-Entry.coefs <- data.frame(coef(mod)$Entry)
-Entry.coefs$Entry <- rownames(Entry.coefs)
+mod <- (mod4)
 
-S <- cbind(rand.cat, Entry.coefs)
-
-### for modBtrophic; skip to line 42 if using modBasic
-mod <- (mod4.1)
 rand.cat <- ddply(data, .(Entry, Study, ExptA, Sys1, TG1), summarize, mean(logY.rs))
 names(rand.cat) <- c('Entry', 'Study', 'ExptA','Syst','TG1', 'meanlogY')
-Entry.coefs <- data.frame(coef(mod4.1)$Entry)
+Entry.coefs <- data.frame(coef(mod)$Entry)
 Entry.coefs$Entry <- rownames(Entry.coefs)
-#S <- cbind(rand.cat, Entry.coefs)
-S <- merge(rand.cat, Entry.coefs, by = 'Entry', all = FALSE) # alternate approach using merge
+S <- merge(rand.cat, Entry.coefs, by = 'Entry', all = FALSE)
 
 ## constructing predicted slopes
 S$Sys.term <- ifelse(S$Syst == 'T', S$logSc.Sys1T, 0)
 S$TG.term <- ifelse(S$TG1 == '2', S$logSc.TG12, 0)
-#S$TG.term <- ifelse(S$TG1 == '3', S$logSc.TG13, S$TG.term)
 S$TG.term <- ifelse(S$TG1 == '4', S$logSc.TG14, S$TG.term)
-S$TG.term <- ifelse((S$Syst=='T' & S$TG1=='4'), (S$logSc.TG14+S$logSc.Sys1T.TG14), S$TG.term)
-#S$units <- ifelse(S$Units == 'density', S$logS.unit.types2density, 0)
-#S$units <- ifelse(S$Units == 'perc.cover', S$logS.unit.types2perc.cover, S$units)
-#S$HT.term <- ifelse(S$HigherT == 'Y', S$logSc.HT, 0)
-#S$Res.term <- ifelse(S$restrt == 'incr', S$logS.restrtincr, 0)
-#S$Res.term <- ifelse(S$restrt == 'red', S$logS.restrtred, S$Res.term)
+S$TG.term <- ifelse((S$Syst=='T' & S$TG1=='4'), (S$logSc.TG14 + S$logSc.Sys1T.TG14), S$TG.term)
 
 #now add Study and ExpA ranefs
-St.ranefs <- data.frame(coef(mod)$Study)
+St.ranefs       <- data.frame(coef(mod)$Study)
 St.ranefs$Study <- rownames(St.ranefs)
-Ex.ranefs  <- data.frame(coef(mod)$ExptA)
+Ex.ranefs       <- data.frame(coef(mod)$ExptA)
 Ex.ranefs$ExptA <- rownames(Ex.ranefs)
-Ent.ranefs  <- data.frame(coef(mod)$Entry)
+Ent.ranefs      <- data.frame(coef(mod)$Entry)
 Ent.ranefs$Entry <- rownames(Ent.ranefs)
 
 St.ranefs1 <- data.frame(St.ranefs$Study, St.ranefs$logSc)
@@ -60,18 +42,21 @@ S2 <- merge(S, St.ranefs1, by.x = 'Study', by.y = 'St.ranefs.Study', all= FALSE)
 S3.1 <- merge(S2, Ex.ranefs1, by.x = 'ExptA', by.y = 'Ex.ranefs.ExptA', all = FALSE)
 S3 <- merge(S3.1, Ent.ranefs1, by.x = 'Entry', by.y = 'Ent.ranefs.Entry', all = FALSE)
 S <- S3
-b <- as.numeric(fixef(mod)[3])
+b <- as.numeric(fixef(mod)[2])
 
-### for modBasic only:
-S$slope <- S$logSc + S$logSc.log.Tscale. + (S$St.ranefs.logSc - b)
+
 
 ### for modBtrophic only: 
-S$slope <- S$logSc + S$Sys.term + S$TG.term + (S$St.ranefs.logSc - b) + (S$Ex.ranefs.logSc - b) + (S$Ent.ranefs.logSc - b)
+S$slope <- S$logSc + S$Sys.term + S$TG.term 
++ (S$St.ranefs.logSc - b) + (S$Ex.ranefs.logSc - b) + (S$Ent.ranefs.logSc - b)
 
+# checking some things:
+length(S[S$Sys.term=='0',]$Sys.term)
+length(S[S$TG.term!='0',]$TG.term)
 
 ## plot histogram of estimated slopes
 #pdf(file='Figure3.pdf', width = 5, height = 3)
-#par(mar = c(4.5,4.5,3,2))
+par(mar = c(4.5,4.5,3,2))
 hist(S$slope, breaks = 40, col = 'gray', freq = TRUE, main = '', xlab = 'Estimated scaling coefficients (b)', xlim = c(-0.4, 1.4), ylim = c(0, 100), cex.lab = 1.2, axes = FALSE, ylab = 'Number of entries') #
 axis(1, at =c(-0.4, -0.2, 0, 0.2, 0.4, 0.6,0.8,1.0,1.2,1.4), lwd = 2, pos = 0)
 axis(2, lwd = 2, pos = -0.4, las = 2)
@@ -123,7 +108,28 @@ summary(S$slope)  # ok, this is genreally working, though something is not quite
 abline(v = mean(S$slope), lwd = 2)
 
 
+#this code used to be at the top to easily allow me to make this figure withi mod2F or mod3F
 
-x <- seq(0, 10, 1)
-modx <- function(x)(yield~0.28*x)
-plot9modx
+#rand.cat <- data.frame(cbind(as.numeric(as.character(data$Entry)), as.numeric(as.character(data$ExptA)), as.numeric(as.character(data$Study))))
+#names(rand.cat) <- c('Entry', 'ExptA', 'Study')
+#rand.cat1 <- ddply(rand.cat, .(Entry, ExptA), summarize, mean(Study)) 
+
+### for modBasic
+rand.cat <- ddply(data, .(Entry, ExptA, Study), summarize, mean(logY.rs))
+names(rand.cat) <- c('Entry','ExptA', 'Study', 'meanlogY')
+Entry.coefs <- data.frame(coef(mod)$Entry)
+Entry.coefs$Entry <- rownames(Entry.coefs)
+
+S <- cbind(rand.cat, Entry.coefs)
+
+### for modBasic only:
+S$slope <- S$logSc + S$logSc.log.Tscale. + (S$St.ranefs.logSc - b)
+
+
+## this code could be added to the slope estimation section to include other model terms:
+
+#S$units <- ifelse(S$Units == 'density', S$logS.unit.types2density, 0)
+#S$units <- ifelse(S$Units == 'perc.cover', S$logS.unit.types2perc.cover, S$units)
+#S$HT.term <- ifelse(S$HigherT == 'Y', S$logSc.HT, 0)
+#S$Res.term <- ifelse(S$restrt == 'incr', S$logS.restrtincr, 0)
+#S$Res.term <- ifelse(S$restrt == 'red', S$logS.restrtred, S$Res.term)
