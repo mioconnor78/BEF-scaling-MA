25caa820b4358159c9e570ef628e2a8808a162ca
Mary <oconnor@zoology.ubc.ca>
2016-01-17 09:09:43 -0800

    got figure working, with all coefficients. had to sort out what was character, numeric, etc.

--- figure 1 code Jan2016.R
+++ figure 1 code Jan2016.R
@@ -23,93 +23,48 @@ Entry.coefs$Entry <- rownames(Entry.coefs)
 S <- merge(rand.cat, Entry.coefs, by = 'Entry', all = FALSE)
 
 b <- as.numeric(fixef(mod4)[2])
+se <- function(x) sd(x)/sqrt(length(x))
 
 ## constructing predicted b values (slopes) from fixed effects for each group.
 ## doing it for TG.term is straighforward, but I think I need to do it for Sys at the same time...
 S$TG.term <- ifelse(S$TG1 == '1', S$logSc, 0)
 S$TG.term <- ifelse(S$TG1 == '2', (S$logSc + S$logSc.TG12), S$TG.term)
 S$TG.term <- ifelse(S$TG1 == '4', (S$logSc + S$logSc.TG14), S$TG.term)
-S$Sys.term <- ifelse(S$Syst == 'T', (S$logSc + S$logSc.Syst1T), S$logSc)
-#S$TG.term <- ifelse((S$Syst=='T' & S$TG1=='4'), (S$logSc + S$logSc.Sys1T.TG14), S$TG.term)
+S$Sys.term <- ifelse(S$Syst == 'T', (S$logSc + S$logSc.Sys1T), S$logSc)
+S$TG.S.int <- ifelse((S$Syst=='T' & S$TG1=='4'), (S$logSc + S$logSc.Sys1T.TG14), S$logSc)
 
-## testing: this produces what I think are the right estimates for trophic groups. whew! So a new figure one, with several panels, I think (?), could this for system and then the interaction (still need to think through the interaction code above; might be right but I need to double check.)
+## testing: this produces what I think are the right estimates for trophic groups. whew! So a new figure one, with several panels, I think (?), could do this for system and then the interaction (still need to think through the interaction code above; might be right but I need to double check.)
 b.sum <- ddply(S, .(TG1), summarize, mean(TG.term))
 b.sum2 <- ddply(S, .(TG1), summarize, se(TG.term))
-b.sums <- merge(b.sum, b.sum2, by = 'TG1')
-b.sums
+b.sumt <- merge(b.sum, b.sum2, by = 'TG1')
+names(b.sumt) <- c('group', 'est', 'se')
+
+b.sumS <- ddply(S, .(Syst), summarize, mean(Sys.term))
+b.sumS2 <- ddply(S, .(Syst), summarize, se(Sys.term))
+b.sumSm <- merge(b.sumS, b.sumS2, by = 'Syst')
+names(b.sumSm) <- c('group', 'est', 'se')
+
+# this isn't working, there is some problem with the class that the final row comes out as. just look at this and simplify it. 
+b.TG.S <- ddply(S, .(Syst, TG1), summarize, mean(TG.S.int))
+b.TG.S2 <- ddply(S, .(Syst, TG1), summarize, se(TG.S.int))
+b.TGS <- cbind(b.TG.S, b.TG.S2)
+b.TGS$group <- paste(b.TGS$Syst, b.TGS$TG1, sep = "")
+names(b.TGS) <- c('Syst', 'TG1', 'est', 'Syst', 'TG1', 'se', 'group')
+b.TGS2 <- as.data.frame(cbind(b.TGS$group, b.TGS$est, b.TGS$se))
+names(b.TGS2) <- c('group', 'est', 'se')
+TGint <- b.TGS2[(b.TGS2$group == 'T4'),]
+
+DO <- c('T4', '-0.31286869838985', '0.0202570171561124')
+
+class(b.sums$group)
+
+row <- (c(as.factor('NA'), 'NA','NA'))
+b.sums <- (rbind(b.sumt, b.sumSm, c(as.factor('T4'), '-0.31286869838985', '0.0202570171561124'))) # just have to add that interaction coefficient, and then the slopes are done. decide on intercepts.
+b.sums$se <- as.numeric(b.sums$se)
+b.sums$est <- as.numeric(b.sums$est)
+b.sums$group <- c("Primary Prod.", "Herbivores", "Detritovores", "Aquatic", "Terrestrial", "Terr.*Detrit.")
 
 
-## Pondering: there might be a S*T category, in which there are the following levels: A*1, A*2, A*4, T*1, T*2, T*4. ?? 
-
-
-#now add Study and ExpA ranefs; all of these are centered on mean of b
-St.ranefs       <- data.frame(coef(mod4)$Study)
-St.ranefs$Study <- rownames(St.ranefs)
-Ex.ranefs       <- data.frame(coef(mod4)$ExptA)
-Ex.ranefs$ExptA <- rownames(Ex.ranefs)
-#Ent.ranefs      <- data.frame(coef(mod4)$Entry)
-#Ent.ranefs$Entry <- rownames(Ent.ranefs)
-
-St.ranefs1 <- data.frame(St.ranefs$Study, St.ranefs$logSc)
-Ex.ranefs1 <- data.frame(Ex.ranefs$ExptA, Ex.ranefs$logSc)
-#Ent.ranefs1 <- data.frame(Ent.ranefs$Entry, Ent.ranefs$logSc)
-
-S2 <- merge(S, St.ranefs1, by.x = 'Study', by.y = 'St.ranefs.Study', all= FALSE)
-S3 <- merge(S2, Ex.ranefs1, by.x = 'ExptA', by.y = 'Ex.ranefs.ExptA', all = FALSE)
-#S3 <- merge(S3.1, Ent.ranefs1, by.x = 'Entry', by.y = 'Ent.ranefs.Entry', all = FALSE)
-S <- S3  ## this is the merged dataframe of fixed and random effects for each group (ExptA)
-
-S$slope <- (S$logSc) + S$TG.term + (S$St.ranefs.logSc - b) + (S$Ex.ranefs.logSc - b)
-#+ S$Sys.term               
-
-     
-## b values for each group (ExptA)
-TG <- ddply(S, .(ExptA, TG1, Syst), summarize, mean(TG.term))
-TG.Sranefs <- ddply(S, .(ExptA, TG1, Syst), summarize, mean(St.ranefs.logSc-b)) ## need to sum ranefs
-TGm <- merge(TG, TG.Sranefs, by = c('ExptA', 'TG1', 'Syst'), all= FALSE)
-TG.Eranefs <- ddply(S, .(ExptA, TG1, Syst), summarize, mean(Ex.ranefs.logSc-b)) ## need to sum ranefs
-TGm2 <- merge(TGm, TG.Eranefs, by = c('ExptA', 'TG1', 'Syst'), all= FALSE)
-names(TGm2) <- c('ExptA', 'TG1', 'Syst', 'fixef.entranef', 'Sranef', 'Eranef')
-
-TG.Entranefs <- ddply(S, .(ExptA, TG1, Syst), summarize, mean(Ent.ranefs.logSc-b)) ## need to sum ranefs
-TG.S <- merge(TGm2, TG.Entranefs, by = c('ExptA', 'TG1', 'Syst'), all= FALSE)
-names(TG.S) <- c('ExptA', 'TG', 'Sys', 'fixef', 'Sranef', 'Eranef', 'Entranef')
-
-TG.S <- TGm2
-TG.S$b <- TG.S$fixef.entranef + TG.S$Sranef + TG.S$Eranef
-
-
-se <- function(x) sd(x)/sqrt(length(x))
-
-b.sum <- ddply(TG.S, .(TG), summarize, mean(b))
-b.sum2 <- ddply(TG.S, .(TG), summarize, se(b))
-b.sums <- merge(b.sum, b.sum2, by = 'TG')
-b.sums
-
-### Estimates still aren't quite right... I'm not sure this is going to work after all. These manually averaged coefficients are reflecting interactions ()
-
-
-
-
-# create data for best model estimates
-est.B <- as.data.frame(as.numeric(round(fixef(mod4),2)))
-est.B$se <- as.numeric(round(sqrt(diag(vcov(mod4))),2))
-names(est.B) <- c('est', 'se')
-rownames(est.B) <- c('Intercept', 'ln(S)', 'Ecosystem', 'Herbivore', 'Detritivore', 'ln(Tg)', 'ln(S)*Ecosystem', 'ln(S)*Herbivore', 'ln(S)*Detritivore','Ecosyst*Detrit', 'ln(S)*Syst*Detrit')
-est.B$slint <- c('I',  'S', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'I', 'S')
-est.B.sl <- est.B[est.B$slint == 'S',]
-est.B.int <- est.B[est.B$slint == 'I',]
-
-
-# create data for basic model estimates  #needs review
-est.Ba <- as.data.frame(as.numeric(round(fixef(mod2F),2)))
-est.Ba$se <- as.numeric(round(sqrt(diag(vcov(mod2F))),2))
-names(est.Ba) <- c('est', 'se')
-est.Ba <- rbind(est.Ba[1,], est.Ba[2,], c('',''), c('',''), c('',''), c('',''), c('',''),  c('',''), c('',''), c('',''), c('','')) 
-rownames(est.Ba) <- c('Intercept', 'ln(S)', 'Ecosystem', 'Herbivore', 'Detritivore', 'ln(Tg)', 'ln(S)*Ecosystem', 'ln(S)*Herbivore', 'ln(S)*Detritivore','Ecosyst*Detrit', 'ln(S)*Syst*Detrit')
-est.Ba$slint <- c('I', 'S', 'I', 'I', 'I', 'I', 'S', 'S', 'S', 'I', 'S')
-est.Ba.sl <- est.Ba[est.Ba$slint == 'S',]
-est.Ba.int <- est.Ba[est.Ba$slint == 'I',]
 
 ### two-paneled figure
 
@@ -125,31 +80,29 @@ par(
 #TOP PANEL: SLOPES
 par(mar=(c(5,9,4,2))) #pin = c(2.3, 3.5), 
 plot(NULL,                                
-     xlim = c(-1.0, 0.6),                        	
-     ylim = c(.7, length(est.B.sl[,1]) + .3), 	
+     xlim = c(-0.4, 0.6),                        	
+     ylim = c(.7, length(b.sums[,1]) + .3), 	
      axes = F, xlab = NA, ylab = NA, cex = 0.8)
 
 # add the data
-ests.B <- as.numeric(est.B.sl[,1])
-ses.B <- as.numeric(est.B.sl[,2])
-ests.Ba <- as.numeric(est.Ba.sl[,1])
-ses.Ba <- as.numeric(est.Ba.sl[,2])
-var.names<-rownames(est.B.sl)
-var.namesi<-rownames(est.B.int)
+ests.B <- (b.sums[,2])
+ses.B <- (b.sums[,3])
+var.names <- (b.sums[,1])
 
-b <- 0
-for (i in 1:length(ests.B)) {                                            
-  points(ests.B[i], i+b, pch = 19, cex = 1.2, col = 1) 
-  lines(c(ests.B[i] + 1.96*ses.B[i], ests.B[i] - 1.96*ses.B[i]), c(i+b, i+b), col = 1, lwd = 2)
-  points(ests.Ba[i], i+2*b, pch = 19, cex = 1.2, col = 'gray50') 
-  lines(c(ests.Ba[i] + 1.96*ses.Ba[i], ests.Ba[i] - 1.96*ses.Ba[i]), c(i+2*b, i+2*b), col = 'gray50', lwd = 2)
-  text(-1.2, i, adj = c(1,0), var.names[i], xpd = T, cex = .8)        # add the variable names
-  text(0.45, length(est.B.sl[,1])+ .2, 'B', cex = 1.2)
+a <- 0
+for (i in 1:length(b.sums[,1])) {                                            
+  points(ests.B[i], i+a, pch = 19, cex = 1.2, col = 1) 
+  lines(c(ests.B[i] + 1.96*ses.B[i], ests.B[i] - 1.96*ses.B[i]), c(i+a, i+a), col = 1, lwd = 2)
+  text(-0.45, i, adj = c(1,0), var.names[i], xpd = T, cex = .8)        # add the variable names
+  text(0.6, length(b.sums[,1])+ .2, 'B', cex = 1.2)
 }
 
 # add axes and labels
 axis(side = 1)                                                                                         
-abline(v = 0, lty = 3, col = "grey40")                                                                   
+#abline(v = 0, lty = 3, col = "grey40")                                                         
+abline(h = 3.5, lty = 3, col = 'grey40')     
+abline(h = 5.5, lty = 3, col = 'grey40')  
+
 mtext(side = 1, "Slope coefficients", line = 3)                                              
 mtext(side = 3, "", line = 1, cex = 0.8)   # add title
 box()                                          
